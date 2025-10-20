ab05a6a3ff93f49989ebd531a8e5bf52e1922da8
Sofiyaan Mohammed <mohammedsofiyaan12@gmail.com>
2025-10-20 17:14:46 +0530

    WIP: Save current responsive/toast notification fixes before switching branch

---
+++ Testing/static/js/upload.js
@@ -0,0 +1,13 @@
+function initUpload() {
+    console.log("Upload Report page initialized.");
+    // This is where you would initialize file drag-and-drop setup
+}
+
+function startProcessing() {
+    // Placeholder function for the upload button
+    showToast("File upload simulated. Processing report...", 'info');
+}
+
+// Expose init function
+window.initUpload = initUpload;
+window.startProcessing = startProcessing;
